15133932fa0d8fe4248c574726c135fbed02b1fb
germanhlab <homoeolabgerman@gmail.com>
2023-09-14 17:52:11 +0530

    Fixed Stuff 4

--- components/ProductDetailsCarousel.jsx
+++ components/ProductDetailsCarousel.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React from "react";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 
